d16eb979254bfed21956c63405c1d107c877737d
GiuseppeRepici <giuseppe.repici95@gmail.com>
2023-06-13 13:24:52 +0200

    completed

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: 'ProjectCard',
+    props: {
+        project: Object
+    }
+}
+</script>
+
+<template>
+    <div class="card h-100">
+        <div class="card-body">
+            <h5>{{ project.name }}</h5>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
